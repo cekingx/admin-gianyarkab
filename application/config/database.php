5d3df1aca50136c5809da5b7d1a007581259e906
Dirga Yasa <cekingx@gmail.com>
2020-09-05 11:09:55 +0800

    Configure to run with localhost:8080

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '127.0.0.1',
 	'username' => 'root',
-	'password' => 'root13',
+	'password' => '',
 	'database' => 'db_gianyarkab',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
